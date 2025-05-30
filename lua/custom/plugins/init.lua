5df0da26ff9363d864af2b2c82648aec43daa156
Karl <karl.neivelt@pm.me>
2025-05-30 13:25:41 +0100

    Update init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,29 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  ---------------------------------------------------------------------------
+  -- Fast virtual-env management with uv
+  {
+    'benomahony/uv.nvim',
+    cmd = { 'UVInit', 'UVRunFile', 'UVSync' },
+    keys = { { '<leader>x', '<cmd>UV<CR>', desc = 'uv menu' } },
+    config = true,
+  },
+
+  ---------------------------------------------------------------------------
+  -- Molten (richer UI)
+  {
+    'benlubas/molten-nvim',
+    build = ':UpdateRemotePlugins',
+    init = function()
+      vim.g.molten_image_provider = 'image.nvim'
+      vim.g.molten_auto_open_output = false
+    end,
+    keys = {
+      { '<leader>jr', ':MoltenEvaluateLine<CR>', desc = 'run line' },
+      { '<leader>jv', ':MoltenEvaluateVisual<CR>gv', mode = 'v', desc = 'run block' },
+      { '<leader>jo', ':MoltenShowOutput<CR>', desc = 'show output' },
+    },
+  },
+}
