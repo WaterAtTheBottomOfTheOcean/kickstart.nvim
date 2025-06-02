4ae304ad4b339f17949bed5525517a2771ffee86
Karl Neivelt <karl.neivelt.24@ucl.ac.uk>
2025-06-02 11:55:27 +0100

    Added small set-up for latex. Removed molten.nvim, too much of a hassle for now to get Jupyter workbooks working

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -12,19 +12,27 @@ return {
     config = true,
   },
 
-  ---------------------------------------------------------------------------
-  -- Molten (richer UI)
-  {
-    'benlubas/molten-nvim',
-    build = ':UpdateRemotePlugins',
+  { -- LaTeX core
+    'lervag/vimtex',
+    lazy = false, -- load immediately
     init = function()
-      vim.g.molten_image_provider = 'image.nvim'
-      vim.g.molten_auto_open_output = false
+      vim.g.vimtex_imaps_enabled = 0 --i.e., disable them
+
+      vim.g.vimtex_view_method = 'zathura'
+      vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
+      --quickfix settings
+      vim.g.vimtex_quickfix_open_on_warning = 0 --  don't open quickfix if there are only warnings
+      vim.g.vimtex_quickfix_ignore_filters =
+        { 'Underfull', 'Overfull', 'LaTeX Warning: .\\+ float specifier changed to', 'Package hyperref Warning: Token not allowed in a PDF string' }
     end,
-    keys = {
-      { '<leader>jr', ':MoltenEvaluateLine<CR>', desc = 'run line' },
-      { '<leader>jv', ':MoltenEvaluateVisual<CR>gv', mode = 'v', desc = 'run block' },
-      { '<leader>jo', ':MoltenShowOutput<CR>', desc = 'show output' },
-    },
+  },
+
+  { -- Citation picker (bib files)
+    'nvim-telescope/telescope-bibtex.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim' },
+    config = function()
+      require('telescope').load_extension 'bibtex'
+      vim.keymap.set('n', '<leader>cc', ':Telescope bibtex<CR>', { desc = 'Insert citation' })
+    end,
   },
 }
